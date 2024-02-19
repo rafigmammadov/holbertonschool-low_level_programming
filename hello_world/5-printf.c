9d13a0a8880bcce3707b44ca6636c829966c47e6
Rafig <rafigmammadov@yahoo.com>
2024-02-19 19:14:19 +0000

    Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints
+ * 'with proper grammar, but the outcome is a piece of art,'
+ * ,followed by a new line.
+ *
+ * Return: Always returns 0
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
