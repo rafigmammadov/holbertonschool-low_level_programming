084ced9ddc81fea7fcfc943ad04ed42381d1aa6e
Rafig <rafigmammadov@yahoo.com>
2024-02-19 10:03:41 +0000

    Hello, puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main- Puts message Programming is like building a multilingual puzzle
+ * , followed by a new line.
+ *
+ * Return: always returns 0
+ */
+
+
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
