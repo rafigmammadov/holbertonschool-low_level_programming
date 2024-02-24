e3647061de9ceeaf948fc5d5adbd63c059e724c8
Rafig <rafigmammadov@yahoo.com>
2024-02-24 10:51:48 +0000

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Function that prints _putchar
+ * followed by a new line.
+ *
+ *
+ * Return: Always returns 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	char str[] = "_putchar";
+
+	while (i < 9)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
