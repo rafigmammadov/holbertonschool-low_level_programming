b5b3c1ae1a8ab02f08d7dd7113f16250917bcbf1
Rafig <rafigmammadov@yahoo.com>
2024-02-24 18:56:03 +0000

    98 Battery Street, the OG

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Function that prints all natural numbers
+ * from n to 98, followed by a new line.
+ *
+ * @n: The parameter tha will be computed
+ *
+ *
+ * Return: Returns no result
+ *
+ */
+
+void print_to_98(int n);
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			_putchar(n + '0');
+
+			if (n == 98)
+			{
+				continue;
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			_putchar(n + '0');
+
+			if (n == 98)
+			{
+				continue;
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
