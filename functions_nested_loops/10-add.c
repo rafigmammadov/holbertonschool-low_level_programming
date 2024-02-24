ef6a943564e19da608ab1842991880e5509cfe3c
Rafig <rafigmammadov@yahoo.com>
2024-02-24 18:44:32 +0000

    a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Function that adds two integers and returns the result.
+ *
+ * @a: The integer that will be operand
+ * @b: The integer that will be operand
+ *
+ * Return: Returns result
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
