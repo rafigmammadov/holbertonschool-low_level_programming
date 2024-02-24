24140140ce810a96df9cc9d29d1ee744710c14d0
Rafig <rafigmammadov@yahoo.com>
2024-02-24 16:25:41 +0000

    Sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -5,8 +5,8 @@
  * @n: The parameter which will be checked for its sign
  *
  * Return: Returns 1 and prints + if n is greater than zero
- * 	   Returns 0 and prints 0 if n is zero
- * 	   Returns -1 and prints - if n is less than zero
+ * 	 Returns 0 and prints 0 if n is zero
+ * 	 Returns -1 and prints - if n is less than zero
  */
 
 int print_sign(int n)
