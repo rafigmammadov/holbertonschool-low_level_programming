30d7fa98476f5eec336bfc8e1e0fa6d57071f85a
Rafig <rafigmammadov@yahoo.com>
2024-02-24 14:41:16 +0000

    10 x alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -15,8 +15,10 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i <= 10; i++)
 	{
+		{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 			_putchar(alpha);
+		}
+		putchar('\n')
 	}
-	_putchar('\n');
 }
