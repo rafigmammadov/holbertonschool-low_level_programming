5263c2a737ad895e41684777adc91abf0c4128b3
Rafig Mammadov <131705272+rafigmammadov@users.noreply.github.com>
2024-02-25 14:02:34 +0400

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -9,34 +9,53 @@
  *
  */
 
+#include <stdio.h>
+
 int main(void)
 {
-	unsigned int count = 0;
-	unsigned long int a, b, c;
+	int count;
+	unsigned long currentFib1 = 0, currentFib2 = 1, sum;
+	unsigned long currentHalf1, currentHalf2, nextHalf1, nextHalf2;
+	unsigned long half1, half2;
 
-	a = 1;
-	b = 2;
 
-	printf("%lu, %lu, ", a, b);
-
-	while (count < 96)
+	for (count = 0; count < 92; count++)
 	{
-		c = a + b;
-		a = b;
-		b = c;
+		sum = currentFib1 + currentFib2;
+		printf("%lu, ", sum);
 
-		if (count == 95)
-		{
-		    printf("%lu", c);
-		    break;
-		}		
-
-		printf("%lu, ", c);
-
-		count++;
+		currentFib1 = currentFib2;
+		currentFib2 = sum;
 	}
 
-	printf("\n");
+	currentHalf1 = currentFib1 / 10000000000;
+	currentHalf2 = currentFib2 / 10000000000;
+	nextHalf1 = currentFib1 % 10000000000;
+	nextHalf2 = currentFib2 % 10000000000;
 
-	return (0);
+
+	for (count = 93; count < 99; count++)
+	{
+		half1 = currentHalf1 + nextHalf1;
+		half2 = currentHalf2 + nextHalf2;
+
+		if (nextHalf2 > 9999999999)
+		{
+			half1 += 1;
+			half2 %= 10000000000;
+        	}
+
+	printf("%lu%lu", half1, half2);
+
+	if (count != 98)
+		printf(", ");
+
+	currentHalf1 = nextHalf1;
+	currentHalf2 = nextHalf2;
+	nextHalf1 = half1;
+	nextHalf2 = half2;
+}
+
+	printf("\n");
+	return 0;
 }
