18570bf061fdf57047032f24fae6331a474f91bb
Rafig <rafigmammadov@yahoo.com>
2024-02-27 19:18:32 +0000

    98 Battery st.

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * reset_to_98 - Function that takes a pointer to an int as
  * parameter and updates the value it points to to 98.
  *
- * @n - The pointer to the integer
+ * @n: The pointer to the integer
  *
  * Return: Returns no value
  *
