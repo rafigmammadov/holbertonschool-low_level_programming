93192b34422628cdb36fca8dd32a9bbfe17de004
Rafig <rafigmammadov@yahoo.com>
2024-02-29 16:57:49 +0000

    Winning is only half of it. Having fun is the other half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -13,13 +13,12 @@
 void puts_half(char *str)
 {
 	int i;
-	int len;
+	int len = 0;
 	int n;
 
-	while (str[i] != '\0')
+	while (str[len] != '\0')
 	{
-		len++;
-		i++;
+		len += 1;
 	}
 
 	for (n = ((len - 1) / 2) + 1; n <= len - 1; n++)
