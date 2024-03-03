2e6bed33918b0c73a4e540dda5672658b1bd60de
Rafig <rafigmammadov@yahoo.com>
2024-03-03 19:46:56 +0000

    Noise is a buffer, more effective than cubicles or booth walls

---
+++ pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_buffer - Function that prints a buffer.
+ * @b: The parameter that represents buffer
+ * @size: The parameter that represents size of buffer
+ * Return: Returns no value
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (j = 0; j < 10 && i + j < size; ++j)
+		{
+			printf("%02x", (unsigned char)b[i + j]);
+			if (j % 2 == 1)
+				printf(" ");
+		}
+
+		for (; j < 10; ++j)
+		{
+			printf("   ");
+			if (j % 2 == 1)
+				printf(" ");
+		}
+
+		for (j = 0; j < 10 && i + j < size; ++j)
+		{
+			char ch = b[i + j];
+
+			if (isprint(ch))
+				printf("%c", ch);
+			else
+				printf(".");
+		}
+		printf("\n");
+	}
+}
