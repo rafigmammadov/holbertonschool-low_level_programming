6d03ea98054c740d91de4c88cdf9e190b7b08dc7
Rafig <rafigmammadov@yahoo.com>
2024-03-05 18:11:50 +0000

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Function that searches a string for any of a set of bytes
+ *
+ * @s: The parameter that represents string
+ * @accept: The parameter that will be searched for occurence
+ *
+ * Return: Returns a pointer to the byte in s that matches
+ * one of the bytes in accept or null
+ *
+ *
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				return (s + i);
+			}
+		}
+	}
+
+	return ('\0');
+}
