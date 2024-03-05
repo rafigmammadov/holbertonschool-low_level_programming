f7f23de4162edcebeaf46b18374679c79e57d789
Rafig <rafigmammadov@yahoo.com>
2024-03-05 19:31:46 +0000

    Chess is mental torture

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Function that prints the chessboard
+ *
+ * @a: The parameter that represents array
+ *
+ * Return: Returns no value
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+	}
+}
