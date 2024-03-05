3fe728d74d6c6037a7c844ab997b80b098da6d6f
Rafig <rafigmammadov@yahoo.com>
2024-03-05 16:21:17 +0000

    memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - Function that copies memory area
+ *
+ * @dest: The parameter that represents destination
+ * @src: The parameter that represents source
+ * @n: The parameter that represents the count of bytes
+ *
+ * Return: Returns a pointer to dest
+ *
+ */
+
+char _memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		src[i] = dest[i];
+	}
+
+	return (dest);
+}
