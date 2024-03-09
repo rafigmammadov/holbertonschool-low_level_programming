2587a04d6ce82071219f8d757e99880c70852e17
Rafig <rafigmammadov@yahoo.com>
2024-03-09 15:44:20 +0000

    To infinity and beyond

---
+++ argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Function that adds positive numbers
+ *
+ * @argc: The parameter that represents the count of arguments
+ * @argv: The parameter that represents the array of the characters
+ *
+ * Return: Always returns 0
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+	int j;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
