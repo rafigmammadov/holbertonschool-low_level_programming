4f7dcfbe753f708e459ec31b58b273bff5747613
Rafig <rafigmammadov@yahoo.com>
2024-03-12 19:12:10 +0000

    He who is not courageous enough to take risks will accomplish nothing in life

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -49,7 +49,7 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0;
 
 	size = _strlen(s1) + _strlen(s2);
-	array = malloc(sizeof(char) * size);
+	array = malloc(sizeof(char) * size + 2);
 
 	if (array == NULL)
 	{
@@ -78,6 +78,8 @@ char *str_concat(char *s1, char *s2)
 
 		i++;
 	}
+
+	*(array + i) = '\0';
 
 	return (array);
 }
