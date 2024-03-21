ff6f27c0eeb5d72e98312535d7948c9675e3e40a
Rafig <rafigmammadov@yahoo.com>
2024-03-21 08:47:31 +0000

    Poppy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - The structure that represents dog
+ *
+ * @name: The parameter that represents name of the dog
+ * @age: The parameter that represents age of the dog
+ * @owner: The parameter that represents owner of the dog
+ *
+ */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog;
+
+#endif
